f5bd931288c79e5977089721307f48a8867e9a6d
Asa Holland <asaleholland@gmail.com>
2022-01-13 23:06:49 -0800

    writing out initial test functions

--- main.py
+++ main.py
@@ -1,2 +1,51 @@
 # main.py
 # Author: Asa LeHolland
+
+import virtualbox
+
+vbox = virtualbox.VirtualBox()
+
+def list_available_machines():
+    return [m.name for m in vbox.machines]
+
+
+def launch_machine(machine_name):
+    session = virtualbox.Session()
+    machine = vbox.find_machine(machine_name)
+    progress = machine.launch_vm_process(session, "gui", [])
+    progress.wait_for_completion()
+
+
+def query_session_state_description(session):
+    session_state = session.state
+
+    session_state_descriptions = {
+        'SessionState(0)': 'null',
+        'SessionState(1)': 'unlocked',
+        'SessionState(2)': 'locked',
+        'SessionState(3)': 'spawning',
+        'SessionState(4)': 'unlocking'
+    }
+
+    if str(session_state) in session_state_descriptions:
+        return session_state_descriptions[session_state]
+    else:
+        return str(session_state)
+
+def query_machine_state_description(machine):
+    machine_state = machine.state
+
+    machine_state_descriptions = {
+        'MachineState(0)': 'null',
+        'MachineState(1)': 'unlocked',
+        'MachineState(2)': 'locked',
+        'MachineState(3)': 'spawning',
+        'MachineState(4)': 'unlocking',
+        'MachineState(5)': 'running'
+    }
+
+    if str(machine_state) in machine_state_descriptions:
+        return machine_state_descriptions[machine_state]
+    else:
+        return str(machine_state)
+
